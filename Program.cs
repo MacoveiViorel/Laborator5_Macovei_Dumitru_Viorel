884595571b8e8444d7d382027c92ae3576fa7a6f
MacoveiViorel <74250108+MacoveiViorel@users.noreply.github.com>
2021-11-15 09:01:03 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator5_Macovei_Dumitru_Viorel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window3D ex = new Window3D();
+            ex.Run(30.0, 0.0);
+        }
+    }
+}
+
